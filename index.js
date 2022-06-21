ca72c2c2d8c8f1d69c11a6c509b0649facd64067
Jnaesi <jacobnasi@hotmail.com>
2022-06-21 21:47:34 +0000

    Add feature to remove a card when clicked

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ cards.forEach((card) => {
     card.addEventListener("click", () => {
         card.classList.add("clicked");
 
-        if(counter === 0) {
+        if (counter === 0) {
             firstSelection = card.getAttribute("animal");
             counter++;
         } else {
             secondSelection = card.getAttribute("animal");
             counter = 0;
 
-            if(firstSelection) === secondSelection) {
-                const correctCards = document.querySelectorAll(".card[animal='" + firstSelection + "']");
+            if(firstSelection === secondSelection) {
+                const correctCards = document.querySelectorAll(".card[animal='" + firstSelection + "']"
+                );
 
-            correctCards[0].classList.add("checked");
-            correctCards[1].classList.add("checked");
+                correctCards[0].classList.add("checked");
+                correctCards[0].classList.remove("clicked");
+                correctCards[1].classList.add("checked");
+                correctCards[1].classList.remove("clicked");
+            } else {
+            } 
         }
     });
-});
+ });  
