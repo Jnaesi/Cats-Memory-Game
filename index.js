6568039c5f98a4cfa976bd32e1f36cc079c84248
Jnaesi <jacobnasi@hotmail.com>
2022-06-22 08:28:08 +0000

    Testing

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ cards.forEach((card) => {
     card.addEventListener("click", () => {
         card.classList.add("clicked");
 
-        if (counter === 0) {
+        if(counter === 0) {
             firstSelection = card.getAttribute("animal");
             counter++;
         } else {
@@ -22,11 +22,19 @@ cards.forEach((card) => {
                 correctCards[0].classList.remove("clicked");
                 correctCards[1].classList.add("checked");
                 correctCards[1].classList.remove("clicked");
+                
             } else {
                 const incorrectCards = document.querySelectorAll(".card.clicked");
 
                 incorrectCards[0].classList.add("shake");
                 incorrectCards[1].classList.add("shake");
+
+                setTimeout(() => {
+                    incorrectCards[0].classList.remove("shake");
+                    incorrectCards[0].classList.remove("clicked");
+                    incorrectCards[1].classList.remove("shake");
+                    incorrectCards[1].classList.remove("clicked");
+                }, 800);
             } 
         }
     });
